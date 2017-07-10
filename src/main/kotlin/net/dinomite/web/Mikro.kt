6458a8d65d040e3a6d5418b622137700658cbe68
Drew Stephens <drew@dinomite.net>
2017-07-10 16:51:52 -0400

    Inline service

--- src/main/kotlin/net/dinomite/web/Mikro.kt
+++ src/main/kotlin/net/dinomite/web/Mikro.kt
@@ -6,26 +6,20 @@ import org.jetbrains.ktor.application.install
 import org.jetbrains.ktor.features.DefaultHeaders
 import org.jetbrains.ktor.logging.CallLogging
 import org.jetbrains.ktor.response.respondText
-import org.jetbrains.ktor.routing.Routing
 import org.jetbrains.ktor.routing.get
+import org.jetbrains.ktor.routing.routing
 
 fun Application.main() {
     install(DefaultHeaders)
     install(CallLogging)
-    install(Routing) {
-        service()
-    }
-}
 
-/**
- * Override this to define your own endpoints
- */
-private fun Routing.service() {
-    get("/{...}") {
-        call.respondText("You should override Routing.service()")
-    }
+    routing {
+        get("/{...}") {
+            call.respondText("You should override Routing.service()")
+        }
 
-    get("/somewhere-else") {
-        call.respondText("This hit /somewhere-else")
+        get("/somewhere-else") {
+            call.respondText("This hit /somewhere-else")
+        }
     }
 }
